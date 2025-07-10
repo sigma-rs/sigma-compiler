f0104198ed882eea75627df32f11022de44d88e7
Ian Goldberg <iang@uwaterloo.ca>
2025-07-10 10:30:11 -0400

    Add sigma_compiler_{prover,verifier} macros to only emit one party's code

--- sigma_compiler_derive/src/lib.rs
+++ sigma_compiler_derive/src/lib.rs
@@ -7,3 +7,15 @@ pub fn sigma_compiler(input: TokenStream) -> TokenStream {
     let mut spec = parse_macro_input!(input as SigmaCompSpec);
     sigma_compiler_core(&mut spec, true, true).into()
 }
+
+#[proc_macro]
+pub fn sigma_compiler_prover(input: TokenStream) -> TokenStream {
+    let mut spec = parse_macro_input!(input as SigmaCompSpec);
+    sigma_compiler_core(&mut spec, true, false).into()
+}
+
+#[proc_macro]
+pub fn sigma_compiler_verifier(input: TokenStream) -> TokenStream {
+    let mut spec = parse_macro_input!(input as SigmaCompSpec);
+    sigma_compiler_core(&mut spec, false, true).into()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub use group;
 pub use rand;
-pub use sigma_compiler_derive::sigma_compiler;
+pub use sigma_compiler_derive::{sigma_compiler, sigma_compiler_prover, sigma_compiler_verifier};
 pub use sigma_rs;
 
 pub mod rangeutils;
