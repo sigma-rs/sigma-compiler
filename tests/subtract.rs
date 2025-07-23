daa01f805a4ad0174c4daba85aebf449dbc7911e
Ian Goldberg <iang@uwaterloo.ca>
2025-07-23 13:52:34 -0400

    Add a test for subtracting a constant from a Scalar

---
+++ tests/subtract.rs
@@ -0,0 +1,26 @@
+#![allow(non_snake_case)]
+use curve25519_dalek::ristretto::RistrettoPoint as G;
+use group::Group;
+use sha2::Sha512;
+use sigma_compiler::*;
+
+#[test]
+fn subtract_test() -> Result<(), sigma_rs::errors::Error> {
+    sigma_compiler! { proof,
+        (x),
+        (C, const cind B),
+        C = (x-1)*B,
+    }
+
+    type Scalar = <G as Group>::Scalar;
+    let mut rng = rand::thread_rng();
+    let B = G::generator();
+    let x = Scalar::random(&mut rng);
+    let C = (x-Scalar::ONE)*B;
+
+    let instance = proof::Instance { C, B };
+    let witness = proof::Witness { x };
+
+    let proof = proof::prove(&instance, &witness, b"subtract_test", &mut rng)?;
+    proof::verify(&instance, &proof, b"subtract_test")
+}
